1f106b91907398edfd3a8aba7066cd63bfdddcab
Vil <101118033+villeneuveandhira@users.noreply.github.com>
2023-04-24 03:06:33 +0700

    Add files via upload

---
+++ Program/synchronization/Controller.java
@@ -0,0 +1,202 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package synchronization;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author Satria Ramadhani
+ */
+public class Controller extends KeyAdapter implements KeyListener
+{
+    /**
+     * Attribute declaration.
+     */
+    
+    private Game game;
+    private Handler handler;
+    private int mark = 0;
+    
+    /**
+     * Constructor.
+     */
+    
+    // Default constructor.
+    public Controller()
+    {
+        this.game = new Game();
+        this.handler = new Handler();
+    }
+    
+    // Constructor with controller data.
+    public Controller(Game game, Handler handler)
+    {
+        this.game = game;
+        this.handler = handler;
+    }
+    
+    /**
+     * Getter and Setter.
+     */
+
+    /* Controller's game. */
+    
+    public Game getGame()
+    {
+        return game;
+    }
+
+    public void setGame(Game game)
+    {
+        this.game = game;
+    }
+
+    /* Controller's handler. */
+    
+    public Handler getHandler()
+    {
+        return handler;
+    }
+
+    public void setHandler(Handler handler)
+    {
+        this.handler = handler;
+    }
+    
+    /**
+     * Public methods.
+     */
+    
+    // Override trait when key is pressed.
+    @Override
+    public synchronized void keyPressed(KeyEvent e)
+    {
+        System.out.println("Pressed");
+        
+        // Get key code (what key that pressed?).
+        int key = e.getKeyCode();
+        int movements = 0;
+        if(game.isRunning())
+        {
+            // Searching for player object.
+            int i = 0; boolean found = false;
+            while((found == false) && (i < handler.count()))
+            {
+                if(handler.get(i).getType().equals("Player"))
+                {
+                    found = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            
+            // Set the object and do the handling.
+            GameObject temp = handler.get(i);
+            if((key == KeyEvent.VK_W) || (key == KeyEvent.VK_UP))
+            {
+                // Move up.
+                movements = 1;
+                temp.setVelY(-5);
+            }
+            if((key == KeyEvent.VK_A) || (key == KeyEvent.VK_LEFT))
+            {
+                // Move left.
+                movements = 2;
+                temp.setVelX(-5);
+            }
+            if((key == KeyEvent.VK_S) || (key == KeyEvent.VK_DOWN))
+            {
+                // Move down.
+                movements = 3;
+                temp.setVelY(+5);
+            }
+            if((key == KeyEvent.VK_D) || (key == KeyEvent.VK_RIGHT))
+            {
+                // Move right.
+                movements = 4;
+                temp.setVelX(+5);
+            }
+            // Print Move
+            if(movements == 1){
+                System.out.println("UP");
+            }
+            if(movements == 2){
+                System.out.println("LEFT");
+            }
+            if(movements == 3){
+                System.out.println("DOWN");
+            }
+            if(movements == 4){
+                System.out.println("RIGHT");
+            }
+            // Check Move
+            if(mark != movements){
+                game.setScore(game.getScore()+1);
+                System.out.println("Score = "+game.getScore());
+                mark = movements; 
+           }
+        }
+    }
+    
+    // Override trait when key is released from being pressed.
+    @Override
+    public synchronized void keyReleased(KeyEvent e)
+    {
+        System.out.println("Released");
+        
+        // Get key code (what key that released?).
+        int key = e.getKeyCode();
+        if(game.isRunning())
+        {
+            // Searching for player object.
+            int i = 0; boolean found = false;
+            while((found == false) && (i < handler.count()))
+            {
+                if(handler.get(i).getType() == "Player")
+                {
+                    found = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            
+            // Set the object and do the handling.
+            GameObject temp = handler.get(i);            
+            if(key == KeyEvent.VK_SPACE)
+            {
+                // Close the game.
+                game.setRunning(false);
+                game.close();
+            }
+            if((key == KeyEvent.VK_W) || (key == KeyEvent.VK_UP))
+            {
+                // Stop from being moved up.
+                temp.setVelY(0);
+            }
+            if((key == KeyEvent.VK_A) || (key == KeyEvent.VK_LEFT))
+            {
+                // Stop from being moved left.
+                temp.setVelX(0);
+            }
+            if((key == KeyEvent.VK_S) || (key == KeyEvent.VK_DOWN))
+            {
+                // Stop from being moved down.
+                temp.setVelY(0);
+            }
+            if((key == KeyEvent.VK_D) || (key == KeyEvent.VK_RIGHT))
+            {
+                // Stop from being moved right.
+                temp.setVelX(0);
+            }
+        }
+    }
+}
